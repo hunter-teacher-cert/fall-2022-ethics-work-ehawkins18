108107607ac643600a45d2b161d69b08dc54ca7d
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2022-09-11 18:20:23 +0000

    updated py

--- py/binsearch.py
+++ py/binsearch.py
@@ -6,3 +6,35 @@
 #
 # Binary Search
 #
+#create random data set
+from random import random
+data = []
+for i in range(10):
+    data = data+[int(10*random()+1)]
+print(data)
+#sort data set
+for i in range(9):
+    for j in range(9):
+        if(data[j+1]<data[j]):
+            temp=data[j]
+            data[j]=data[j+1]
+            data[j+1]=temp
+print(data)
+#binary search function: find index of first instance of tgt in data
+def binarySearch(tgt,data):
+    min=0
+    max=len(data)-1
+    middle = int((min+max)/2)
+    while (min < max):
+        if (data[middle]==tgt):
+            return middle
+        elif (data[middle]<tgt):
+            min = middle
+            middle = int((min+max)/2)
+        else:
+            max = middle
+            middle = int((min+max)/2)
+#call binarySearch and print result
+print(binarySearch(5,data))
+    
+
