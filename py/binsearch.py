82e93bd006e32e1eea3d14028461b5cc804f9e5d
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2022-09-11 17:03:08 +0000

    updated py

---
+++ py/binsearch.py
@@ -0,0 +1,8 @@
+# filename: binsearch.py
+# Ed Hawkins
+# CSCI 77800 Fall 2022
+# collaborators: me, myself, I
+# consulted: ThinkPython
+#
+# Binary Search
+#
---
+++ py/gol.py
@@ -0,0 +1,8 @@
+# filename: cgol.py
+# Ed Hawkins
+# CSCI 77800 Fall 2022
+# collaborators: me, myself, I
+# consulted: ThinkPython
+#
+# Conway's Game of Life
+#
