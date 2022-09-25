fb5dbf05075994d6c73e8b76100a417c783b501f
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2022-09-25 15:27:41 +0000

    updated regex.py

--- week04/regex.py
+++ week04/regex.py
@@ -7,27 +7,35 @@ def find_name(line):
     #original pattern from Z's code
    # result = re.findall(pattern,line)
 
+    #First Last
     pattern=r'[A-Z]\w*\s[A-Z]\w*'
     result = re.findall(pattern,line)
-  
+
+    #First Middle Last
     pattern=r'[A-Z]\w*\s[A-Z]\w*\s[A-Z]\w*'
     result = result + re.findall(pattern,line)
 
+    #F. M. Last
     pattern=r'[A-Z]\.\s[A-Z]\.\s[A-Z]\w*'
     result = result + re.findall(pattern,line)
-  
+
+    #Dr. Last
     pattern=r'Dr.\s[A-Z]\w*'
     result = result + re.findall(pattern,line)
-  
+
+    #Mr. Last
     pattern=r'Mr.\s[A-Z]\w*'
     result = result + re.findall(pattern,line)
-  
+
+    #Mrs. Last
     pattern=r'Mrs.\s[A-Z]\w*'
     result = result + re.findall(pattern,line)
 
+    #Ms. F. Last
     pattern=r'Ms.\s[A-Z]\.\s[A-Z]\w*'
     result = result + re.findall(pattern,line)
 
+    #Mrs. F. Last
     pattern=r'Mrs.\s[A-Z]\.\s[A-Z]\w*'
     result = result + re.findall(pattern,line)
     return result
